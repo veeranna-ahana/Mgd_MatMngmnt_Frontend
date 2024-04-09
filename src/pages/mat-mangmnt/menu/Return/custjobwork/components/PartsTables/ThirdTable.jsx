afa3c024341cb141bc06b88d1da67187dffb4847
ADITYA KUMAR <CodifyWithAditya@Gmail.com>
2024-04-09 22:28:12 +0530

    table sorting in part all three tables...adi 9april24

--- src/pages/mat-mangmnt/menu/Return/custjobwork/components/PartsTables/ThirdTable.jsx
+++ src/pages/mat-mangmnt/menu/Return/custjobwork/components/PartsTables/ThirdTable.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import Table from "react-bootstrap/Table";
+
+import { FaArrowUp } from "react-icons/fa";
+
 export default function ThirdTable(props) {
   function changeQTY(key, val) {
     // console.log("fdsffsfddsfadsfdafdsbv", key);
@@ -15,6 +18,56 @@ export default function ThirdTable(props) {
 
     props.setThirdTableData(arr);
   }
+
+  const sortedData = () => {
+    let dataCopy = [...props.thirdTableData];
+
+    if (props.sortConfigThird.key) {
+      dataCopy.sort((a, b) => {
+        if (
+          !parseFloat(a[props.sortConfigThird.key]) ||
+          !parseFloat(b[props.sortConfigThird.key])
+        ) {
+          // console.log("string");
+          if (a[props.sortConfigThird.key] < b[props.sortConfigThird.key]) {
+            return props.sortConfigThird.direction === "asc" ? -1 : 1;
+          }
+          if (a[props.sortConfigThird.key] > b[props.sortConfigThird.key]) {
+            return props.sortConfigThird.direction === "asc" ? 1 : -1;
+          }
+          return 0;
+        } else {
+          // console.log("number");
+          if (
+            parseFloat(a[props.sortConfigThird.key]) <
+            parseFloat(b[props.sortConfigThird.key])
+          ) {
+            return props.sortConfigThird.direction === "asc" ? -1 : 1;
+          }
+          if (
+            parseFloat(a[props.sortConfigThird.key]) >
+            parseFloat(b[props.sortConfigThird.key])
+          ) {
+            return props.sortConfigThird.direction === "asc" ? 1 : -1;
+          }
+          return 0;
+        }
+      });
+    }
+
+    return dataCopy;
+  };
+
+  const requestSort = (key) => {
+    let direction = "asc";
+    if (
+      props.sortConfigThird.key === key &&
+      props.sortConfigThird.direction === "asc"
+    ) {
+      direction = "desc";
+    }
+    props.setSortConfigThird({ key, direction });
+  };
   return (
     <>
       <Table
@@ -25,13 +78,49 @@ export default function ThirdTable(props) {
         <thead className="tableHeaderBGColor">
           <tr>
             <th>SL No</th>
-            <th>Part ID</th>
-            <th>Return</th>
-            <th>Remarks</th>
+            <th onClick={() => requestSort("PartIdNew")} className="cursor">
+              Part ID
+              <FaArrowUp
+                className={
+                  props.sortConfigThird.key === "PartIdNew"
+                    ? props.sortConfigThird.direction === "desc"
+                      ? "rotateClass"
+                      : ""
+                    : "displayNoneClass"
+                }
+              />
+            </th>
+            <th
+              onClick={() => requestSort("QtyReturnedNew")}
+              className="cursor"
+            >
+              Return
+              <FaArrowUp
+                className={
+                  props.sortConfigThird.key === "QtyReturnedNew"
+                    ? props.sortConfigThird.direction === "desc"
+                      ? "rotateClass"
+                      : ""
+                    : "displayNoneClass"
+                }
+              />
+            </th>
+            <th onClick={() => requestSort("Remarks")} className="cursor">
+              Remarks
+              <FaArrowUp
+                className={
+                  props.sortConfigThird.key === "Remarks"
+                    ? props.sortConfigThird.direction === "desc"
+                      ? "rotateClass"
+                      : ""
+                    : "displayNoneClass"
+                }
+              />
+            </th>
           </tr>
         </thead>
         <tbody>
-          {props.thirdTableData.map((val, key) => (
+          {sortedData().map((val, key) => (
             <tr key={key}>
               <td>{key + 1}</td>
               <td>{val.PartIdNew}</td>
